9633ef618fb746d6a90fd0683ed84a2c71c6567f
basakkturkerr <basakk.turkerr@gmail.com>
2019-07-13 19:17:18 +0300

    initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. sayiyi giriniz: ");
+            int sayi1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("2. sayiyi giriniz: ");
+            int sayi2 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("3. sayiyi giriniz: ");
+            int sayi3 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine((sayi1+sayi2) * sayi3);
+            Console.ReadLine();
+        }
+        
+    }
+}
